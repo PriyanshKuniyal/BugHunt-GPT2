b96b1ed2686282bfb05621c8abf1e6cfd7db17d3
Priyansh Kuniyal <priyanshkuniyal.developer@gmail.com>
2025-08-09 19:09:53 +0530

    Update web_browser.py

--- utils/web_browser.py
+++ utils/web_browser.py
@@ -27,8 +27,7 @@ class TextBasedBrowser:
         )
         
         # Random human-like user agent
-        ua = UserAgent()
-        user_agent = ua.random
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
         
         self.context = self.browser.new_context(
             user_agent=user_agent,
